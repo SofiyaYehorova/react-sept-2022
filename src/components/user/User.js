27cba79d62f165d73e7ab82431561b855e822a35
SofiyaYehorova <yehorova66@gmail.com>
2023-01-04 12:52:06 +0200

    lesson_1

---
+++ src/components/user/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function User(props) {
+    let {item:user} = props;
+    return (
+        <div>
+            <h2>{user.id} {user.name}</h2>
+            <p>{user.address.city} <br/> {user.email}</p>
+
+        </div>
+    );
+}
+
+export default User;
---
+++ src/components/users/Users.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import User from "../user/User";
+
+function Users() {
+    let [users, setUsers] = useState([]);
+
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+        .then(response=>response.json())
+        .then(data=>{
+            setUsers(data);
+        });
+    return (
+        <div>
+            {
+                users.map((user, index)=>(<User item={user} key={index}/>))
+            }
+        </div>
+    );
+}
+
+export default Users;
