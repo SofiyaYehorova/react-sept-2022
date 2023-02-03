dbc1536785fa78e4cc47ff0b9c903823953f6cca
SofiyaYehorova <yehorova66@gmail.com>
2023-02-03 19:01:18 +0200

    hw_4_context_children

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import {MyContext} from "../../index";
+import {Button} from "../../components";
+
 
 const AboutPage = () => {
+    const context = useContext(MyContext);
+    context.gender = 'male';
+    Object.assign(context, {status: true})
+    delete context.name
     return (
         <div>
+            <Button click={() => console.log('Click from customer button!')} style={{backgroundColor: 'black'}}
+            >click</Button>
             AboutPage
         </div>
     );
--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -6,8 +6,9 @@ const UsersPage = () => {
     const context = useContext(MyContext);
     return (
         <div>
-            <div>name:{context.name}</div>
-            <div>age:{context.age}</div>
+            {/*<div>name:{context.name}</div>*/}
+            {/*<div>age:{context.age}</div>*/}
+            {JSON.stringify(context)}
         </div>
     );
 };
