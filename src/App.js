1017b8b5ae3eaf8f8d5ca7a811dd6581898cc637
SofiyaYehorova <yehorova66@gmail.com>
2023-02-06 13:45:05 +0200

    lesson_5_component_life_cycle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-function App() {
-  return (
-    <div className="App">
+import {useState} from "react";
 
-    </div>
-  );
+import {ClassComponent} from "./components/ClassComponent";
+import {FunctionComponent} from "./components/FunctionComponent";
+
+function App() {
+    const [flag, setFlag] = useState(true);
+    return (
+        <div className="App">
+            {/*{flag && <ClassComponent name={'max'}/>}*/}
+            {flag && <FunctionComponent name={'max'}/>}
+            <button onClick={() => setFlag(prev => !prev)}>hide</button>
+        </div>
+    );
 }
 
 export {
-  App
+    App
 }
