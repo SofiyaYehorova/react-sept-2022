62e7281161c1dfd6959b8bc2c8f372a551506af0
SofiyaYehorova <yehorova66@gmail.com>
2023-02-28 14:07:32 +0200

    lesson_8

--- interfaces/userInterface.ts
+++ interfaces/userInterface.ts
@@ -1,36 +1,36 @@
 // export interface IAddress{
-//     "street": string,
-//         "suite": string,
-//         "city": string,
-//         "zipcode": string,
-//         "geo": {
-//             "lat": string,
-//             "lng": string
+//     street: string,
+//         suite: string,
+//         city: string,
+//         zipcode: string,
+//         geo: {
+//             lat: string,
+//             lng: string
 //         }
 // }
 //
 // export interface IUser {
-//     "id": number,
-//     "name": string,
-//     "username": string,
-//     "email": string,
-//     "address": IAddress
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: IAddress
 // }
 
-export interface IUser {
-    id: number,
-    name: string,
-    username: string,
-    email: string,
-    address: {
-        street: string,
-        suite: string,
-        city: string,
-        zipcode: string,
-        geo: {
-            lat: string,
-            lng: string
-        }
-    }
-}
+// export interface IUser {
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: {
+//         street: string,
+//         suite: string,
+//         city: string,
+//         zipcode: string,
+//         geo: {
+//             lat: string,
+//             lng: string
+//         }
+//     }
+// }
 
